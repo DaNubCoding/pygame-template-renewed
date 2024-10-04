196c1d4c129e0faa425d31a9e8590878289a29ee
DaNubCoding <danubcoding@gmail.com>
2024-10-04 16:55:08 -0400

    render cleanup temporary removal of extra render layer types

--- src/core/_types.py
+++
@@ -1,4 +0,0 @@
-import pygame
-
-ColorType = tuple[int, int, int] | tuple[int, int, int, int] | pygame.Color
-RectType = tuple[int, int, int, int] | pygame.Rect
--- src/core/render_layer.py
+++ src/core/render_layer.py
@@ -1,24 +1,25 @@
 from src.core.sprite import Sprite
-
-from typing import Callable, Iterable
-from enum import Enum
+from typing import Callable
+from enum import Enum, auto
 import pygame
+
 
 class RenderLayer:
     def __init__(self, key: Callable[[Sprite], int] = None) -> None:
         self.key = key
-        self.sprites: list[Sprite] = []
+        self.updating_sprites: list[Sprite] = []
+        self.rendering_sprites: list[Sprite] = []
         self.bound_sprites: dict[Sprite, Sprite] = {}
 
     def update(self, dt: float) -> None:
-        if self.key is not None:
-            self.sprites.sort(key=self.key)
-
-        for sprite in self.sprites:
+        for sprite in self.updating_sprites:
             sprite.update(dt)
 
     def draw(self, screen: pygame.Surface) -> None:
-        for sprite in self.sprites:
+        if self.key is not None:
+            self.rendering_sprites.sort(key=self.key)
+
+        for sprite in self.rendering_sprites:
             self._draw_sprite(sprite, screen)
 
     def _draw_sprite(self, sprite: Sprite, screen: pygame.Surface) -> None:
@@ -29,10 +30,14 @@ class RenderLayer:
             self._draw_sprite(self.bound_sprites[sprite], screen)
 
     def add(self, sprite: Sprite) -> None:
-        self.sprites.append(sprite)
+        self.updating_sprites.append(sprite)
+        if isinstance(sprite, Sprite):
+            self.rendering_sprites.append(sprite)
 
     def remove(self, sprite: Sprite) -> None:
-        self.sprites.remove(sprite)
+        self.updating_sprites.remove(sprite)
+        if isinstance(sprite, Sprite):
+            self.rendering_sprites.remove(sprite)
 
     def bind(self, bottom: Sprite, top: Sprite) -> None:
         if top in self.bound_sprites:
@@ -47,7 +52,7 @@ class RenderLayer:
             self.remove(top)
         except ValueError:
             # top either doesn't exist in the layer or has been removed
-            # from being bound to another sprite
+            # from being having been bound to another sprite
             pass
         self.bound_sprites[bottom] = top
 
@@ -58,31 +63,10 @@ class RenderLayer:
         return False
 
     def __len__(self) -> int:
-        return len(self.sprites) + len(self.bound_sprites)
+        return len(self.updating_sprites) + len(self.bound_sprites)
 
-class FilteredRenderLayer(RenderLayer):
-    def __init__(self, filter: Callable[[Sprite], bool]) -> None:
-        super().__init__()
-        self.filter = filter
-
-    def update(self, dt: float) -> None:
-        for sprite in filter(self.filter, self.sprites):
-            sprite.update(dt)
-
-class SelectiveRenderLayer(RenderLayer):
-    def __init__(self, selector: Callable[[RenderLayer], Iterable[Sprite]]) -> None:
-        super().__init__()
-        self.selector = selector
-
-    def update(self, dt: float) -> None:
-        for sprite in self.selector(self):
-            sprite.update(dt)
-
-    def draw(self, screen: pygame.Surface) -> None:
-        for sprite in self.selector(self):
-            self._draw_sprite(sprite, screen)
 
 class Layer(Enum):
-    BACKGROUND = RenderLayer
-    DEFAULT = RenderLayer
-    HUD = RenderLayer
+    BACKGROUND = auto()
+    DEFAULT = auto()
+    HUD = auto()
