49537198ab036d4a3f1593228b72797db49afe8a
DaNubCoding <danubcoding@gmail.com>
2024-12-20 17:04:15 -0500

    change dt to be pixels/second

--- src/core/game.py
+++ src/core/game.py
@@ -18,7 +18,7 @@ class Game:
         self.screen = pygame.display.set_mode((960, 540), SCALED | FULLSCREEN * (not Debug.on()))
         self.clock = pygame.time.Clock()
 
-        self.dt = self.clock.tick(60) * 0.05
+        self.dt = self.clock.tick(0) / 1000
         self.scene = MainScene(self)
 
     def run(self) -> None:
@@ -36,7 +36,7 @@ class Game:
 
             pygame.display.set_caption(f"FPS: {self.clock.get_fps():.0f}")
 
-            self.dt = self.clock.tick(0) * 0.05
+            self.dt = self.clock.tick(0) / 1000
 
         pygame.quit()
 
--- src/sprites/player.py
+++ src/sprites/player.py
@@ -15,7 +15,7 @@ class Player(Sprite):
         self.pos = Vec(0, 0)
 
     def update(self, dt: float) -> None:
-        self.pos.x += 1 * dt
+        self.pos.x += 100 * dt
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.rect(screen, (255, 0, 0), (*self.pos, 50, 50))
