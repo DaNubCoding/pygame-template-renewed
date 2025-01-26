6c010297a4b4ccfe660afe25d74e66c363d4469e
DaNubCoding <danubcoding@gmail.com>
2025-01-25 23:26:12 -0500

    fix timer issues - LoopTimer not stopping properly after max_loops reached - Pausing having unintended side-effects - .elapsed no longer continue increasing after the timer is done - and more small fixes

--- src/game/sprites/player.py
+++ src/game/sprites/player.py
@@ -6,14 +6,17 @@ class Player(Sprite):
         self.pos = Vec(0, 0)
         self.image = Image.get("player")
         self.angle = 0
-        self.timer = Timer(3)
+        self.timer = LoopTimer(2.5, max_loops=3)
 
     def update(self, dt: float) -> None:
-        self.pos = self.game.mouse_pos + Vec(50, 50) * self.timer.done
+        self.pos = self.game.mouse_pos + Vec(20, 0) * self.timer.loops
         if self.game.mouse_pressed[0]:
             self.angle += 90 * dt
         else:
             self.angle -= 90 * dt
+
+        if self.game.key_down == pygame.K_SPACE:
+            self.timer.toggle()
 
     def draw(self, screen: pygame.Surface) -> None:
         self.image = pygame.transform.rotate(Image.get("player"), self.angle)
